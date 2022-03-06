c24d2b8a455656651518b52f2722ee32e6a5dde9
factuall <63786641+factuall@users.noreply.github.com>
2022-03-06 17:31:32 +0100

    styling and cleanup

--- popup-gui.js
+++ popup-gui.js
@@ -1,27 +1,27 @@
 var stateBox = document.getElementById("state");
-var tasksStepsList = document.getElementById("taskStepsList");
-var optionInputs = document.getElementsByClassName('optionInput');
+var tasksStepsList = document.getElementById("task-steps-list");
+var optionInputs = document.getElementsByClassName('option-input');
 var taskSteps = [];
 
 //record button
-var startRecButton = document.getElementById("startRecording");
+var startRecButton = document.getElementById("start-recording");
 startRecButton.addEventListener('click', async () =>{ 
     sendToAddon("input-recording");
 });
 
 //stop button clicked
-var stopRecButton = document.getElementById("stopRecording");
+var stopRecButton = document.getElementById("stop-recording");
 stopRecButton.addEventListener('click', async () =>{
     sendToAddon("input-stop");
 });
 
 //execute button clicked
-var executeTaskButton = document.getElementById("doTask");
+var executeTaskButton = document.getElementById("do-task");
 executeTaskButton.addEventListener('click', async () =>{
     sendToAddon("input-play");
 });
 
-var clearStepsButton = document.getElementById("clearRecording");
+var clearStepsButton = document.getElementById("clear-recording");
 clearStepsButton.addEventListener('click', async () =>{
     taskSteps = [];
     renderSteps();
@@ -41,19 +41,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 function renderSteps(){
     tasksStepsList.innerHTML = "";
-    //gowno kurwa na szybko for title
-    if(taskSteps.length > 0) {
-        let stepLine = document.createElement("div");
-        stepLine.append("Steps in given task:");
-        tasksStepsList.append(stepLine);
-        let gowno = document.createElement("div");
-        gowno.style.height = "5px";
-        tasksStepsList.append(gowno);
+
+    let listLabel = document.createElement("div");
+    listLabel.append("Steps list:");
+    listLabel.style.padding = "5px 5px 2px 0px";
+    tasksStepsList.append(listLabel);
+
+    if(taskSteps.length == 0){
+        let emptyReminder = document.createElement("div");
+        emptyReminder.id = "step-line";
+        tasksStepsList.append(emptyReminder);
+        emptyReminder.append("(list is currently empty)");
+        emptyReminder.style.display="inline-block";
+        listLabel.style.display="inline-block";
     }
     //adding each task step
     taskSteps.forEach(element => {
         let stepLine = document.createElement("div");
-        stepLine.id = "stepLine";
+        stepLine.id = "step-line";
         tasksStepsList.append(stepLine);
         stepLine.append("Wait: " + element.wait/1000 + "s then click on X:" + element.left + " Y:" + element.top);
     });
